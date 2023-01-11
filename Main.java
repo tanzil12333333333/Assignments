1b44f6a2b37c1627c57aa0a5b68dbe419f3fdf3c
tanzil12333333333 <114287025+tanzil12333333333@users.noreply.github.com>
2023-01-11 18:50:55 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int num = 899, sum = 9;
+
+        for(int i = 1; i <= num; ++i)
+        {
+
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
